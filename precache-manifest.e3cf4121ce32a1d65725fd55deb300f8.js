7a16925f48894a4ba004c967987141dd7193a6dc
Gábor, Kállay <kallay.gabesz@gmail.com>
2020-05-26 21:29:22 +0200

    Updates

---
+++ precache-manifest.e3cf4121ce32a1d65725fd55deb300f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54765f44280695b91c6b01326c4db7a9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "dfa415363f3f9f52564c",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "893129dda626ccadf31a",
+    "url": "/robofriends/static/css/main.e3b8cfa9.chunk.css"
+  },
+  {
+    "revision": "dfa415363f3f9f52564c",
+    "url": "/robofriends/static/js/2.1868894d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.1868894d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893129dda626ccadf31a",
+    "url": "/robofriends/static/js/main.f7e96daa.chunk.js"
+  },
+  {
+    "revision": "75ddd68c1bd8a525031a",
+    "url": "/robofriends/static/js/runtime-main.9d7464c9.js"
+  }
+]);
