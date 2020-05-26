233e472622a53fd3269a010691aa62e2f7410381
Gábor, Kállay <kallay.gabesz@gmail.com>
2020-05-26 21:23:00 +0200

    init commit for robofriends for try out github pages

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class ErrorBoundry extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({hasError: true});
+    }
+
+    render() {
+        return this.state.hasError ? <h1>Ooooops. That is not good!</h1> : this.props.children;
+    }
+
+}
+
+export default ErrorBoundry;
---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
+    }
+
+    render() {
+        const {robots, searchField} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        return !robots.length
+            ? <h1>Loading</h1>
+            : (<div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>)
+    }
+}
+
+export default App;
